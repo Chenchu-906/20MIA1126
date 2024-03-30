fd8359407e78648bbf31bb4afa6969e09cf5775d
Chenchu-906 <70853410+Chenchu-906@users.noreply.github.com>
2024-03-30 16:31:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductsList from './pages/ProductsList';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductsList />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
